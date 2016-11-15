ef06116fdda376bdd3a0aacfd925f4896453caac
niaiwoya <2636075448@qq.com>
2016-11-15 23:19:48 +0800

    i love you

--- dsOne/src/main/java/dsOne/GoodTime.java
+++ dsOne/src/main/java/dsOne/GoodTime.java
@@ -3,6 +3,10 @@ package dsOne;
 public class GoodTime {
 	public void getTime(){
 		System.out.println("你好和高新见到你！！！！！");
+		System.out.println("你好和高新见到你！！！！！");
+		System.out.println("你好和高新见到你！！！！！");
+		System.out.println("你好和高新见到你！！！！！");
+		
 	}
 
 }
