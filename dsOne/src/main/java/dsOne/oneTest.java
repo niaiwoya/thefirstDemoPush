0e1ca2781723432f9b5a66150c0a9627ed357a65
niaiwoya <2636075448@qq.com>
2016-11-15 21:48:14 +0800

    事实上色戒色戒中的问题

--- dsOne/src/main/java/dsOne/oneTest.java
+++ dsOne/src/main/java/dsOne/oneTest.java
@@ -3,7 +3,12 @@ package dsOne;
 public class oneTest {
 	public void nihao (){
 		System.out.println("this is a boy !!!");
-		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		
 		System.out.println("this is a boy !!!");
 		System.out.println("this is a boy !!!");
 		//System.out.println("time is my time!!!");
