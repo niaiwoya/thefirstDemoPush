4434831e357d1b10efefab4bcc79fb67325910e6
niaiwoya <2636075448@qq.com>
2016-11-15 21:47:18 +0800

    窝窝窝窝

--- dsOne/src/main/java/dsOne/oneTest.java
+++ dsOne/src/main/java/dsOne/oneTest.java
@@ -3,7 +3,7 @@ package dsOne;
 public class oneTest {
 	public void nihao (){
 		System.out.println("this is a boy !!!");
-		System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
 		System.out.println("this is a boy !!!");
 		System.out.println("this is a boy !!!");
 		//System.out.println("time is my time!!!");
