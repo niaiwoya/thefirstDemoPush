b38df79c79004136533a9b9300d896e3f3b8f148
niaiwoya <2636075448@qq.com>
2016-11-24 15:38:26 +0800

    add a four class

--- dsOne/src/main/java/dsOne/TestTime.java
+++ dsOne/src/main/java/dsOne/TestTime.java
@@ -7,6 +7,13 @@ public class TestTime {
 	public void getTiem(){
 		System.out.println("zhe shi shi dang qian de time");
 		Logger.getLogger("nihao");
+		
+		
+		System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");System.out.println("this is a boy !!!");
+		System.out.println("this is a boy !!!");
 	}
 
 }
