7675fe4c1cead0f4f91f9a2e818dffbc2db8f598
niaiwoya <2636075448@qq.com>
2016-11-24 15:06:19 +0800

    tian jia le yi ge clss

---
+++ dsOne/src/main/java/dsOne/TestTime.java
@@ -0,0 +1,12 @@
+package dsOne;
+
+import java.util.logging.Logger;
+
+public class TestTime {
+	
+	public void getTiem(){
+		System.out.println("zhe shi shi dang qian de time");
+		Logger.getLogger("nihao");
+	}
+
+}
